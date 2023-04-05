02be6eca54f3cf2139b63caa275ee96005f6d15d
karolinam11 <124812776+karolinam11@users.noreply.github.com>
2023-04-05 20:18:17 +0200

    Calculator

--- app/src/main/java/com/example/maligiganci/Calculator.kt
+++ app/src/main/java/com/example/maligiganci/Calculator.kt
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class Kalkulator : AppCompatActivity() {
+class Calculator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kalkulator)
 
     val button = findViewById<View>(R.id.button)
     button.setOnClickListener {
-        val intent = Intent(this@Kalkulator, ChooseMode::class.java)
+        val intent = Intent(this@Calculator, ChooseMode::class.java)
         startActivity(intent) }
     }
 }
