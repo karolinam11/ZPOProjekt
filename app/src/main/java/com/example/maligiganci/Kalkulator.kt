4b0ff012f5d1231996aa5621e15f3351582f69f1
karolinam11 <124812776+karolinam11@users.noreply.github.com>
2023-04-06 11:28:00 +0200

    you_lose and draw activity

--- app/src/main/java/com/example/maligiganci/Kalkulator.kt
+++ app/src/main/java/com/example/maligiganci/Kalkulator.kt
@@ -1,16 +1,18 @@
 package com.example.maligiganci
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
 class Kalkulator : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kalkulator)
 
-    val button = findViewById<View>(R.id.button)
+    val button = findViewById<View>(R.id.button_play)
     button.setOnClickListener {
         val intent = Intent(this@Kalkulator, ChooseMode::class.java)
         startActivity(intent) }
--- app/src/main/java/com/example/maligiganci/TicTacToe.kt
+++ app/src/main/java/com/example/maligiganci/TicTacToe.kt
@@ -1,6 +1,7 @@
 package com.example.maligiganci
 
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,11 +9,12 @@ import android.view.View
 
 
 class TicTacToe : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tic_tac_toe)
 
-        val button = findViewById<View>(R.id.button)
+        val button = findViewById<View>(R.id.button_play)
         button.setOnClickListener {
             val intent = Intent(this@TicTacToe, ChooseMode::class.java)
             startActivity(intent) }
