ae46a1126522f86c283b14f854f11d332ce38a8c
karolinam11 <124812776+karolinam11@users.noreply.github.com>
2023-04-30 17:21:46 +0200

    started tic tac toe

--- app/src/main/java/com/example/maligiganci/GameTicTacToe.kt
+++ app/src/main/java/com/example/maligiganci/GameTicTacToe.kt
@@ -1,40 +1,16 @@
 package com.example.maligiganci
 
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.maligiganci.databinding.ActivityGameTicTacToeBinding
+import android.os.Bundle
+import android.view.View
 
 class GameTicTacToe : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityGameTicTacToeBinding
-
+    enum class
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityGameTicTacToeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_game_tic_tac_toe)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        setContentView(R.layout.activity_game_tic_tac_toe)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_game_tic_tac_toe)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
+    fun boardTapped(view: View) {}
 }
