ee5a280c17473ab1542f4a685d90f7e42dfdcb08
karolinam11 <124812776+karolinam11@users.noreply.github.com>
2023-04-30 19:23:45 +0200

    GameTicTacToe

--- app/src/main/java/com/example/maligiganci/TicTacToe.kt
+++ app/src/main/java/com/example/maligiganci/TicTacToe.kt
@@ -1,22 +1,21 @@
 package com.example.maligiganci
 
-
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 
-
 class TicTacToe : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tic_tac_toe)
+        Log.d("TAG", "GameTicTacToe onCreate called")
 
-        val button = findViewById<View>(R.id.button_play)
+        val button = findViewById<View>(R.id.button_play1)
         button.setOnClickListener {
-            val intent = Intent(this@TicTacToe, ChooseMode::class.java)
-            startActivity(intent) }
+            val intent = Intent(this@TicTacToe, GameTicTacToe::class.java)
+            startActivity(intent)
+        }
     }
 }
