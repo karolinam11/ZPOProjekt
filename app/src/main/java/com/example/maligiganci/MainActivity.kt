b44419aa2b9d2a6c223d33744561966d96516bf9
Sebastian Bednarski <sebo21cc21@gmail.com>
2023-12-05 03:11:08 +0100

    Add parents block baby

--- app/src/main/java/com/example/maligiganci/MainActivity.kt
+++ app/src/main/java/com/example/maligiganci/MainActivity.kt
@@ -7,7 +7,11 @@ import android.view.View
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
@@ -30,6 +34,32 @@ class MainActivity : AppCompatActivity() {
             textView.text = user?.email
             saveEmailToFirebase(user?.email)
         }
+
+        val flagRef = FirebaseDatabase.getInstance().getReference("blockBaby/flag")
+
+        flagRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val flag = dataSnapshot.getValue(Boolean::class.java)
+
+                if (flag == false) {
+                    // If flag is false, block the activities and show the lock message
+                    findViewById<View>(R.id.imageButton1).isEnabled = false
+                    findViewById<View>(R.id.imageButton2).isEnabled = false
+                    findViewById<View>(R.id.imageButton3).isEnabled = false
+                    findViewById<TextView>(R.id.block).text = "BLOKADA RODZICIELSKA"
+                } else {
+                    // If flag is true, enable the activities
+                    findViewById<View>(R.id.imageButton1).isEnabled = true
+                    findViewById<View>(R.id.imageButton2).isEnabled = true
+                    findViewById<View>(R.id.imageButton3).isEnabled = true
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+        })
+
 
         val button = findViewById<View>(R.id.imageButton1)
         button.setOnClickListener {
